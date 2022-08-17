e4a90f75948449f13d38f475aec3329706e55f3b
Luke <103851112+moeshy@users.noreply.github.com>
2022-08-17 11:09:37 +0100

    setup

---
+++ coc-game/src/Components/App/App.js
@@ -0,0 +1,15 @@
+import Footer from '../Footer';
+import NavBar from '../Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <body className="App">
+    <NavBar/>
+      <h1 className="main-header"> COC Game </h1>
+      <Footer/>
+    </body>
+  );
+}
+
+export default App;
---
+++ coc-game/src/Components/Footer/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p>Copyright &copy; Clash of Cli-Mates</p>
+    </div>
+  );
+}
